5586eb35a161987f56228de1e964f08f73705a18
Sebastian Vogel <s.vogel@gentics.com>
2013-04-04 22:49:24 +0200

    improved tests

--- test/currencyconverttest.js
+++ test/currencyconverttest.js
@@ -1,13 +1,22 @@
+var chai = require("chai"),
+	assert = chai.assert,
+	currencyConverter = require('../lib/currencyconverter');
+
 describe('CurrencyConverter', function () {
 	"use strict";
 	describe('#convert()', function () {
 		it('should convert without error', function (done) {
-			var currencyConverter = require('../lib/currencyconverter');
 			currencyConverter.convert("EUR", "USD", 10.5).then(function () {
 				done();
-			}, function (err) {
-				throw err;
-			});
+			}, done);
+		});
+		it('should return a number which is greater than 0', function (done) {
+			currencyConverter.convert("GBP", "USD", 10.5).then(function (result) {
+				assert.isNumber(result, "the result is a number");
+				assert(!isNaN(result), "the result is not NaN");
+				assert(result > 0, "the result is greater than 0");
+				done();
+			}, done).fail(done);
 		});
 	});
 });
